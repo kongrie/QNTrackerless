49d2a53139d36b021963bdbd9b24a336197d6a0d
Fe3O4_Git <Fe3O4_Live@Outlook.com>
2021-09-20 23:58:02 +0800

    clean HookEntry

--- app/src/main/java/org/element/qntrackerless/HookEntry.java
+++ app/src/main/java/org/element/qntrackerless/HookEntry.java
@@ -29,8 +29,6 @@ import nil.nadph.qnotified.util.Utils;
 
 public class HookEntry implements IXposedHookLoadPackage {
     public static final String PACKAGE_NAME_QQ = "com.tencent.mobileqq";
-    public static final String PACKAGE_NAME_QQ_INTERNATIONAL = "com.tencent.mobileqqi";
-    public static final String PACKAGE_NAME_QQ_LITE = "com.tencent.qqlite";
     public static final String PACKAGE_NAME_TIM = "com.tencent.tim";
     public static final String PACKAGE_NAME_SELF = "org.element.qntrackerless";
 
@@ -40,7 +38,6 @@ public class HookEntry implements IXposedHookLoadPackage {
             XposedBridge.log("package id must NOT be 0x7f, reject loading...");
             return;
         }
-        //dumpProcessInfo(lpparam.isFirstApplication);
         switch (lpparam.packageName) {
             case PACKAGE_NAME_SELF:
                 XposedHelpers.findAndHookMethod("nil.nadph.qnotified.util.Utils", lpparam.classLoader, "getActiveModuleVersion", XC_MethodReplacement.returnConstant(Utils.QN_VERSION_NAME));
@@ -49,41 +46,6 @@ public class HookEntry implements IXposedHookLoadPackage {
             case PACKAGE_NAME_TIM:
                 StartupHook.getInstance().doInit(lpparam.classLoader);
                 break;
-            case PACKAGE_NAME_QQ_INTERNATIONAL:
-            case PACKAGE_NAME_QQ_LITE:
-                //coming...
         }
     }
-
-//    public static void dumpProcessInfo(boolean i) {
-//        int pid = android.os.Process.myPid();
-//        String name = "unknown";
-//        BufferedReader cmdlineReader = null;
-//        try {
-//            cmdlineReader = new BufferedReader(new InputStreamReader(
-//                    new FileInputStream(
-//                            "/proc/" + pid + "/cmdline"),
-//                    "iso-8859-1"));
-//            int c;
-//            StringBuilder processName = new StringBuilder();
-//            while ((c = cmdlineReader.read()) > 0) {
-//                processName.append((char) c);
-//            }
-//            name = processName.toString();
-//            if (name.contains(":")) name = name.split(":")[1];
-//            else name = "main";
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (cmdlineReader != null) {
-//                try {
-//                    cmdlineReader.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        android.util.Log.i("Xposed", "doInit(i=" + i + ") called @ " + pid + ":" + name);
-//        android.util.Log.i("QNdump", "doInit(i=" + i + ") called @ " + pid + ":" + name);
-//    }
 }
